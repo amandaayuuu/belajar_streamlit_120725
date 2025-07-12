17bd85647b8e81ff54afc288263c486bf29c1955
amandaayuuu <zeaanissaa@gmail.com>
2025-07-12 09:19:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,3 +23,19 @@ st.line_chart(chart_data)
 
 if st.button("Say hello"):
     st.write("Why hello there")
+
+import streamlit as st
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
